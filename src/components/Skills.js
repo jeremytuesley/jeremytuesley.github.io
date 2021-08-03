08215f5df1ecd1c0f566559948d54a0a5f724576
jeremytuesley <jeremytuesley@gmail.com>
2021-08-03 18:31:01 +1000

    created skills page

---
+++ src/components/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div className="content">
+      <div className="skills-page">
+        <h1 className="skills-title">Tech Stack</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
