b2f33dba606cfff1e5d20e02d867844fa564396d
jeremytuesley <jeremytuesley@gmail.com>
2021-08-04 17:54:08 +1000

    Languages styled and added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,19 +13,46 @@ const Skills = () => {
         <div>
           <h2 className="skills-content-title">Languages</h2>
           <div className="skills-content-container">
-            <div className="skills">HTML</div>
-            <div className="skills">CSS</div>
-            <div className="skills">Sass</div>
-            <div className="skills">JavaScript</div>
-            <div className="skills">TypeScript</div>
-            <div className="skills">React Js</div>
-            <div className="skills">Ruby</div>
-            <div className="skills">Ruby on Rails</div>
+            <div className="skills">
+              <i class="fab fa-html5"></i>
+            </div>
+            <div className="skills">
+              <i class="fab fa-css3-alt"></i>
+            </div>
+            <div className="skills">
+              <i class="fab fa-sass"></i>
+            </div>
+            <div className="skills">
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"
+                alt="JavaScriptLogo"
+              />
+            </div>
+            <div className="skills">
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-256/typescript-3521774-2945272.png"
+                alt="TypeScriptLogo"
+              />
+            </div>
+            <div className="skills">
+              <i class="fab fa-react"></i>
+            </div>
+            <div className="skills">
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-128/ruby-3521683-2945127.png"
+                alt="RubyLogo"
+              />
+            </div>
+            <div className="skills">
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-128/rails-3629008-3030233.png"
+                alt="RailsLogo"
+              />
+            </div>
           </div>
         </div>
-        <div className="skills-content-seperator" />
         <div>
-          <h2 className="skills-content-title">Other Technologies</h2>
+          <h2 className="skills-content-title">Other Tech</h2>
           <div className="skills-content-container">
             <div className="skills">Apollo GraphQL</div>
             <div className="skills">Axios</div>
