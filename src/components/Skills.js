b3b95539c9c79effa388895f0b51c4bb85a8ad3d
jeremytuesley <jeremytuesley@gmail.com>
2021-08-05 16:17:16 +1000

    Typography styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,60 +8,168 @@ const Skills = () => {
   return (
     <div className="skills-page">
       <a name="skills" />
-      <h1 className="skills-title">My Technology Stack</h1>
+      <h1 className="skills-title">
+        Here's my <span className="title-colour">Tech Stack</span>
+      </h1>
       <div className="skills-outer-container">
         <div>
           <h2 className="skills-content-title">Languages</h2>
           <div className="skills-content-container">
             <div className="skills">
-              <i class="fab fa-html5"></i>
+              <div className="image-container">
+                <i class="fab fa-html5"></i>
+              </div>
+              <h2 className="skill-name">HTML5</h2>
             </div>
             <div className="skills">
-              <i class="fab fa-css3-alt"></i>
+              <div className="image-container">
+                <i class="fab fa-css3-alt"></i>
+              </div>
+              <h2 className="skill-name">CSS3</h2>
             </div>
             <div className="skills">
-              <i class="fab fa-sass"></i>
+              <div className="image-container">
+                <i class="fab fa-sass"></i>
+              </div>
+              <h2 className="skill-name">SCSS</h2>
             </div>
             <div className="skills">
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"
-                alt="JavaScriptLogo"
-              />
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"
+                  alt="JavaScriptLogo"
+                />
+              </div>
+              <h2 className="skill-name">JavaScript</h2>
             </div>
             <div className="skills">
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-256/typescript-3521774-2945272.png"
-                alt="TypeScriptLogo"
-              />
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-256/typescript-3521774-2945272.png"
+                  alt="TypeScriptLogo"
+                />
+              </div>
+              <h2 className="skill-name">TypeScript</h2>
             </div>
             <div className="skills">
-              <i class="fab fa-react"></i>
+              <div className="image-container">
+                <i class="fab fa-react"></i>
+              </div>
+              <h2 className="skill-name">React Js</h2>
             </div>
             <div className="skills">
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-128/ruby-3521683-2945127.png"
-                alt="RubyLogo"
-              />
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/ruby-3521683-2945127.png"
+                  alt="RubyLogo"
+                />
+              </div>
+              <h2 className="skill-name">Ruby</h2>
             </div>
             <div className="skills">
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-128/rails-3629008-3030233.png"
-                alt="RailsLogo"
-              />
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/rails-3629008-3030233.png"
+                  alt="RailsLogo"
+                />
+              </div>
+              <h2 className="skill-name">Ruby on Rails</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://d33wubrfki0l68.cloudfront.net/a1da522d0a3057a1bc3fb411fcbbf57a447c1146/65e71/img/symbol/svg/full_colored_dark.svg"
+                  alt="BashLogo"
+                />
+              </div>
+              <h2 className="skill-name">Bash</h2>
             </div>
           </div>
         </div>
         <div>
-          <h2 className="skills-content-title">Other Tech</h2>
+          <h2 className="skills-content-title">Tools</h2>
           <div className="skills-content-container">
-            <div className="skills">Apollo GraphQL</div>
-            <div className="skills">Axios</div>
-            <div className="skills">Cypress</div>
-            <div className="skills">Figma</div>
-            <div className="skills">Git</div>
-            <div className="skills">Heroku</div>
-            <div className="skills">PostgreSQL</div>
-            <div className="skills">Mongo DB</div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/rest-api.png"
+                  alt="RestApiLogo"
+                />
+              </div>
+              <h2 className="skill-name">Rest Api</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/apollo-graphql-compact.svg"
+                  alt="ApolloLogo"
+                />
+              </div>
+              <h2 className="skill-name">Apollo GQL</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Axios_logo_%282020%29.svg/300px-Axios_logo_%282020%29.svg.png"
+                  alt="AxiosLogo"
+                />
+              </div>
+              <h2 className="skill-name">Axios</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://www.cypress.io/static/33498b5f95008093f5f94467c61d20ab/c0bf4/cypress-logo.webp"
+                  alt="CypressLogo"
+                />
+              </div>
+              <h2 className="skill-name">Cypress</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/figma-3629363-3032356.png"
+                  alt="FigmaLogo"
+                />
+              </div>
+              <h2 className="skill-name">Figma</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/git-225996.png"
+                  alt="GitLogo"
+                />
+              </div>
+              <h2 className="skill-name">Git</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/heroku-2752161-2284978.png"
+                  alt="HerokuLogo"
+                />
+              </div>
+              <h2 className="skill-name">Heroku</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/postgresql-3521647-2945091.png"
+                  alt="PostgreSQLLogo"
+                />
+              </div>
+              <h2 className="skill-name">PostgreSQL</h2>
+            </div>
+            <div className="skills">
+              <div className="image-container">
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-128/mongodb-226029.png"
+                  alt="MongoDBLogo"
+                />
+              </div>
+              <h2 className="skill-name">MongoDB</h2>
+            </div>
           </div>
         </div>
       </div>
